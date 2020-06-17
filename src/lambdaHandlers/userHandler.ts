4568fef63bbd3ac612708ac685e4c7ca332ddacb
Lorenzo Rodrigo <lrodrigo@gmail.com>
2020-06-17 15:48:45 +1000

    Added headers

--- src/lambdaHandlers/userHandler.ts
+++ src/lambdaHandlers/userHandler.ts
@@ -27,6 +27,10 @@ export const registration: APIGatewayProxyHandler = async (event) => {
 
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
       body: JSON.stringify(response),
     };
   } catch (error) {
